756d83e278acfde3bee8fc6f99b278777f345c27
0xFF <45086516+bqio@users.noreply.github.com>
2022-09-05 14:34:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import xml.etree.ElementTree as ET
 import json
+import argparse
 
 FORUM_ID = 1605
 
 context = None
 output = []
 
-for event, elem in ET.iterparse("rutracker-20220528.xml", events=("start", "end")):
+parser = argparse.ArgumentParser(description="Rutracker parser by bqio")
+
+parser.add_argument("-f", dest="file", required=True, type=str, help="rutracker dump file")
+parser.add_argument("-i", dest="forumid", required=True, type=int, help="rutracker forum id")
+
+args = parser.parse_args()
+
+for event, elem in ET.iterparse(args.file, events=("start", "end")):
   if event == "start":
     if elem.tag == "torrent" and context == None:
       context = elem
   if event == "end":
     if elem == context:
       forum_id = elem[2].attrib.get("id")
-      if int(forum_id) == FORUM_ID:
+      if int(forum_id) == args.forumid:
         title = elem[0].text
         hash = elem[1].attrib.get("hash")
         output.append({
           "title": title,
           "hash": hash,
         })
+        print(title)
       context = None
       elem.clear()
       #files = []
