e2776a208c28f6eb5f9fae943e53a084272e6cc1
bqio <dmitry.mikhaltsov98@gmail.com>
2024-03-25 07:01:45 +0300

    small refactoring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import Iterator
 
 
 def iterparse(xml_path: str) -> Iterator[tuple[str, ET.Element]]:
-    return ET.iterparse(xml_path, events=('start', 'end'))
+    return ET.iterparse(xml_path, events=("start", "end"))
 
 
 def to_tracker_url(id: int) -> str:
@@ -16,6 +16,10 @@ def to_tracker_url(id: int) -> str:
         return f"http://bt{id}.t-ru.org/ann?magnet"
 
 
+if len(sys.argv) != 3:
+    print("Usage: main.py xml_path forum_id")
+    exit(0)
+
 dest = []
 context = iterparse(sys.argv[1])
 context = iter(context)
@@ -23,29 +27,29 @@ context = iter(context)
 event, root = context.__next__()
 
 for event, elem in context:
-    if event == 'end' and elem.tag == 'torrent':
-        title_elem = elem.find('title')
-        forum_elem = elem.find('forum')
-        torrent_elem = elem.find('torrent')
-        del_elem = elem.find('del')
+    if event == "end" and elem.tag == "torrent":
+        title_elem = elem.find("title")
+        forum_elem = elem.find("forum")
+        torrent_elem = elem.find("torrent")
+        del_elem = elem.find("del")
 
-        if title_elem != None and forum_elem != None and torrent_elem != None and del_elem == None:
-            forum_id = forum_elem.attrib.get('id')
+        if (
+            title_elem != None
+            and forum_elem != None
+            and torrent_elem != None
+            and del_elem == None
+        ):
+            forum_id = forum_elem.attrib.get("id")
             title = title_elem.text
-            hash = torrent_elem.attrib.get('hash')
-            tracker = to_tracker_url(
-                int(torrent_elem.attrib.get('tracker_id')))
+            hash = torrent_elem.attrib.get("hash")
+            tracker = to_tracker_url(int(torrent_elem.attrib.get("tracker_id")))
 
             if forum_id == sys.argv[2]:
-                entry = {
-                    'title': title,
-                    'hash': hash,
-                    'tracker': tracker
-                }
+                entry = {"title": title, "hash": hash, "tracker": tracker}
                 dest.append(entry)
-                print(title, hash, tracker)
+                print(entry)
 
         root.clear()
 
-with open(f"forum_{sys.argv[2]}.json", "w") as fp:
+with open(f"forum_{sys.argv[2]}.json", "w", encoding="utf-8") as fp:
     json.dump(dest, fp)
